75566cb7b464eef42c186846212d4957e704f2ca
mrbrunelli <matheus.brunelli@gmail.com>
2023-09-18 17:38:17 -0300

    remove sha 256 digest

--- src/main/java/com/github/mrbrunelli/keycloak/aes256/AES256PasswordHashProvider.java
+++ src/main/java/com/github/mrbrunelli/keycloak/aes256/AES256PasswordHashProvider.java
@@ -1,6 +1,5 @@
 package com.github.mrbrunelli.keycloak.aes256;
 
-import org.keycloak.common.util.Base64;
 import org.keycloak.credential.hash.PasswordHashProvider;
 import org.keycloak.models.PasswordPolicy;
 import org.keycloak.models.credential.PasswordCredentialModel;
@@ -9,12 +8,12 @@ import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.security.Key;
-import java.security.MessageDigest;
 import java.security.spec.AlgorithmParameterSpec;
+import java.util.Base64;
 
 public class AES256PasswordHashProvider implements PasswordHashProvider {
-    private static final String SECRET_KEY = "771a1cf03e8c7934";
-    private static final String SALT = "ecf35be993624dc5";
+    private static final String ENCRYPTION_KEY = "771a1cf03e8c7934";
+    private static final String ENCRYPTION_IV = "ecf35be993624dc5";
 
     private final int defaultIterations;
     private final String providerId;
@@ -42,12 +41,11 @@ public class AES256PasswordHashProvider implements PasswordHashProvider {
         try {
             Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
 
-            MessageDigest md = MessageDigest.getInstance("SHA-256");
-            Key key = new SecretKeySpec(md.digest(SECRET_KEY.getBytes("UTF-8")), "AES");
-            AlgorithmParameterSpec iv = new IvParameterSpec(SALT.getBytes("UTF-8"));
+            Key key = new SecretKeySpec(ENCRYPTION_KEY.getBytes("UTF-8"), "AES");
+            AlgorithmParameterSpec iv = new IvParameterSpec(ENCRYPTION_IV.getBytes("UTF-8"));
 
             cipher.init(Cipher.ENCRYPT_MODE, key, iv);
-            return Base64.encodeBytes(cipher.doFinal(rawPassword.getBytes()));
+            return Base64.getEncoder().encodeToString(cipher.doFinal(rawPassword.getBytes()));
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
